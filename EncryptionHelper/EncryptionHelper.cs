ee1d7c4e788afcf80cb914266cdcce02c0cbba68
Glen Wilkin <glen@luthansolutions.com>
2024-12-12 20:01:05 +0000

    Add project files.

---
+++ EncryptionHelper/EncryptionHelper.cs
@@ -0,0 +1,42 @@
+﻿namespace EncryptionHelper;
+
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public static class EncrytptionHelper
+{
+    private static readonly byte[] Key = Encoding.UTF8.GetBytes("Your32CharSecureKeyHere123456789"); // Replace with a secure key
+    private static readonly byte[] IV = Encoding.UTF8.GetBytes("Your16CharIVHere"); // Replace with a secure IV
+
+    public static string Encrypt(string? plainText)
+    {
+        using var aes = Aes.Create();
+        aes.Key = Key;
+        aes.IV = IV;
+        var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+        using var ms = new MemoryStream();
+        using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+        {
+            using var writer = new StreamWriter(cs);
+            writer.Write(plainText);
+        }
+
+        return Convert.ToBase64String(ms.ToArray());
+    }
+
+    public static string Decrypt(string? cipherText)
+    {
+        using var aes = Aes.Create();
+        aes.Key = Key;
+        aes.IV = IV;
+        var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+        using var ms = new MemoryStream(Convert.FromBase64String(cipherText!));
+        using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+        using var reader = new StreamReader(cs);
+        return reader.ReadToEnd();
+    }
+}
---
+++ EncryptionUnitTests/EncryptionHelperTests.cs
@@ -0,0 +1,21 @@
+﻿namespace EncryptionUnitTests;
+
+using EncryptionHelper;
+using FluentAssertions;
+
+public class EncryptionHelperTests
+{
+    [Theory]
+    [InlineData("Glen Wilkin")]
+    [InlineData("")]
+    [InlineData("True")]
+    [InlineData("1234567890")]
+    [InlineData("public void CorrectlyEncryptsAndDecrypts(string? stringToEncrypt)\r\n{\r\nvar encryptedText = EncrytptionHelper.Encrypt(stringToEncrypt);\r\nvar actual = EncrytptionHelper.Decrypt(encryptedText);\r\n\r\n_ = actual.Should().Be(stringToEncrypt);\r\n}")]
+    public void CorrectlyEncryptsAndDecrypts(string? stringToEncrypt)
+    {
+        var encryptedText = EncrytptionHelper.Encrypt(stringToEncrypt);
+        var actual = EncrytptionHelper.Decrypt(encryptedText);
+
+        _ = actual.Should().Be(stringToEncrypt);
+    }
+}
